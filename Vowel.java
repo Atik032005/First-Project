7c1f51de711003296e852334c64217e9216c44c3
Atik032005 <atik032005@gmail.com>
2026-09-24 00:45:19 +0600

    Add files via upload

---
+++ Vowel.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Vowel {
+    public static void main(String [] args) {
+
+        char ch;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a chracter:");
+        ch = input.next().charAt(0);
+
+        if(ch == 'a') {
+            System.out.println("Vowel");
+        }
+
+        else if(ch == 'e') {
+            System.out.println("Vowel");
+        }
+
+        else if(ch == 'i') {
+            System.out.println("Vowel");
+        }
+
+        else if(ch == 'o') {
+            System.out.println("Vowel");
+        }
+
+        else if(ch == 'u') {
+            System.out.println("Vowel");
+        }
+
+        else {
+            System.out.println("Consonant");
+        }
+
+
+    }
+    
+}
